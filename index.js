b95efaf9f5084563a59d5a262838352003729cd1
lisswart <arnoudlisa@gmail.com>
2021-03-31 02:20:04 -0400

    commit tree a08f672930f9b1601c45e157de5e971e2133cd76

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Write your solution here!
+const drivers = ['Milo', 'Otis', 'Garfield'];
+
+function destructivelyAppendDriver(name) {
+    return drivers.push(name);
+}
+
+function destructivelyPrependDriver(name) {
+    return drivers.unshift(name);
+}
+
+function destructivelyRemoveLastDriver() {
+    drivers.pop();
+    return drivers;
+}
+
+function destructivelyRemoveFirstDriver() {
+    drivers.shift();
+    return drivers;
+}
+
+function appendDriver(name) {
+    const newDrivers = drivers;
+    return newDrivers.concat(name);
+}
+
+function prependDriver(name) {
+    const newDrivers = drivers;
+    return [name, ...newDrivers];
+}
+
+function removeLastDriver() {
+    const newDrivers = drivers;
+    return newDrivers.slice(0, newDrivers.length - 1);
+}
